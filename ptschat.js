fd5fbcc8b0ef4b1dc3b63f6cd6711ee6fb213d9b
DeBraid <de.braid@gmail.com>
2014-06-10 20:47:52 -0400

    initial commit

---
+++ ptschat.js
@@ -0,0 +1,88 @@
+// explicit global
+Messages = new Meteor.Collection('Messages');
+
+var getCurrEmail = function () {
+  return Meteor.user() &&
+    Meteor.user().emails &&
+    Meteor.user().emails[0].address;
+};
+
+if (Meteor.isClient) {
+  
+  Template.messages.allMessages = function () {
+    // passing no args to find, it will return all
+    return Messages.find({}, {
+      sort: {score: -1}
+    });
+  },
+
+  Template.messages.showArrow = function () {
+    return Meteor.userId() && 
+      ! _.contains(this.votes, Meteor.userId());
+  },
+
+
+  Template.messages.userId = function () {
+    return Meteor.userId();
+  },
+
+  Template.messages.events({
+    // map from css selector to behaviour
+    "click .vote": function (evt, templ) {
+      Messages.update(this._id, {
+
+        $inc: {score: 1}, 
+        $addToSet: {votes: Meteor.userId()}
+
+      })
+    },
+
+    "click #messageSubmit": function (evt, templ) {
+
+      var message = templ.find('#messageText').value;
+
+      Messages.insert({
+        message: message, 
+        score: 1,
+        email: getCurrEmail(),
+        votes: [Meteor.userId()] 
+
+      });
+    }
+  })
+}
+
+if (Meteor.isServer) {
+  
+  Messages.allow({
+  
+    insert: function (userId, doc) {
+
+      if ( !_.isEqual(doc.votes, [userId]) ) {
+        return false;
+      }
+      if (!doc.email || !doc.message) {
+        return false;
+      }
+      if (doc.score !== 1) {
+        return false
+      }
+      return true
+    },
+
+    update: function (userId, doc, fieldNames, modifier) { 
+      return _.isEqual(modifier, {
+        $inc: {score: 1},
+        $addToSet: {votes: Meteor.userId()}
+      });
+    }
+  })
+}
+
+
+
+
+
+
+
+
